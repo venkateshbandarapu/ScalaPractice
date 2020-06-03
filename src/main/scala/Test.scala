7cab2299338323530546d053e9142bb3efbb97ef
venkateshbandarapu <venki.b321@gmail.com>
2020-06-03 16:33:46 +0530

    initial commit

---
+++ src/main/scala/Test.scala
@@ -0,0 +1,85 @@
+import java.sql.Timestamp
+import java.util
+import java.util.Calendar
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.{SparkSession, functions}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{DataTypes, StructField, StructType, TimestampType}
+
+
+object Test{
+
+  def main(args: Array[String]): Unit = {
+
+    val getEmpSchema=StructType(Array(StructField("emp_id",DataTypes.IntegerType),
+      StructField("emp_name",DataTypes.StringType),
+      StructField("salary",DataTypes.DoubleType),
+      StructField("join_date",DataTypes.TimestampType)))
+
+    val spark=SparkSession.builder()
+      .master("local[*]")
+    //  .enableHiveSupport()
+      .getOrCreate();
+
+    Logger.getLogger("org").setLevel(Level.OFF);
+    Logger.getLogger("akka").setLevel(Level.OFF);
+
+
+   val empData= spark
+      .read
+      .option("header", "true")
+      .schema(getEmpSchema)
+      .csv("src/main/resources/emp_joindate.csv");
+
+    val groupedData=empData
+      .orderBy(functions.asc("join_date"))
+      .groupBy("emp_id")
+      .agg(functions.collect_list(empData.col("join_date")).as("list_join_dates"))
+
+   val validgroupedData= groupedData
+     .withColumn("valid_join_dates", getValidSvcDatesUDF(groupedData.col("list_join_dates")))
+
+   val empDataWithSvcDt= validgroupedData
+      .withColumn("svc_start_dt",explode(col("valid_join_dates")))
+      .drop("list_join_dates", "valid_join_dates").dropDuplicates
+    empDataWithSvcDt.printSchema()
+    empDataWithSvcDt.show(false)
+
+  }
+
+  val getValidSvcDatesUDF=udf(getValidSvcDates)
+
+  def getValidSvcDates() = (svcDates:Seq[Timestamp])=>{
+
+    var validJoinDates:List[Timestamp]=List.empty
+    for (i <- svcDates.indices) {
+      val joinDT = svcDates(i)
+
+      println("length:"+validJoinDates.length)
+      if (svcDates.length == 1)
+        validJoinDates:+ joinDT
+      else {
+        var recentTS = if (i == 0) joinDT
+        else  validJoinDates.last
+
+        val cal = Calendar.getInstance
+        cal.setTime(recentTS)
+        cal.add(Calendar.DAY_OF_WEEK, 30)
+        val time_30 = new Timestamp(cal.getTime.getTime)
+        if (recentTS.before(joinDT) && time_30.after(joinDT)) {
+          println("inside if")
+          validJoinDates=validJoinDates:+ recentTS
+        }
+        else {
+          println("inside else")
+          validJoinDates=validJoinDates:+joinDT
+        }
+      }
+    }
+    validJoinDates
+
+  }
+
+
+}
